76760ddbb1f7900a2ebd3d00cb6d8586b5c881c6
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2024-08-25 13:12:22 -0700

    Construct random engine

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main () {
 	cout << "Hello!\n\n";
+	cout << "Welcome to GenSystem Version 0.01!" << endl;
+	cout << "(c) 2024 Giancarlo Whitaker" << endl << endl;
+
+	// construct random engine
+	int seed = 333;  // non-random seed for testing
+	default_random_engine engine(seed);
 
 	return 0;
 }
