8f063010d9f63badfd4a5b407ffdc776f7f3727a
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2024-08-25 17:50:03 -0700

    Add <star.h>

---
+++ Star.cpp
@@ -0,0 +1,144 @@
+#include <utility>  // for overloading remaining comparison operators
+#include <string>
+#include <iostream>
+#include "Star.h"
+using namespace std;
+
+Star::Star()
+{
+	//ctor
+}
+
+Star::Star(const Star& other) {
+	mass = other.mass;
+	age = other.age;
+	metallicity = other.metallicity;
+	temperature = other.temperature;
+	luminosity = other.luminosity;
+	radius = other.radius;
+}
+
+//alternate constructor
+Star::Star(double m)
+{
+    mass = m;
+}
+
+///////////////////////////////////////
+// ACCESSORS
+///////////////////////////////////////
+
+double Star::GetMass () {
+	return mass;
+}
+
+double Star::GetAge () {
+	return age;
+}
+
+double Star::GetMetallicity () {
+	return metallicity;
+}
+
+double Star::GetTemperature () {
+	return temperature;
+}
+
+double Star::GetLuminosity () {
+	return luminosity;
+}
+
+double Star::GetRadius () {
+	return radius;
+}
+
+/*char Star::GetSpectralClass () const
+{   // BAFGKMLTDX
+    switch (spectral) {
+        case 0 : return 'B';
+        case 1 : return 'A';
+        case 2 : return 'F';
+        case 3 : return 'G';
+        case 4 : return 'K';
+        case 5 : return 'M';
+        case 6 : return 'L';
+        case 7 : return 'T';
+        case 8 : return 'D';
+        case 9 : return 'X';
+    }
+}
+
+int Star::GetSubClass () const
+{
+    return sub;
+}
+
+string Star::GetLuminosityClass () const
+{   // III, IV, V
+    switch (luminos) {
+        case 0 : return "III";
+        case 1 : return "IV";
+        case 2 : return "V";
+        case -1 : return "";
+    }
+}
+
+string GetStar ()
+{
+    string sp, sb, l;
+    sp = GetSpectralClass();
+    sb = GetSubClass();
+    l = GetLuminosityClass();
+
+}*/
+
+///////////////////////////////////////
+// MUTATORS
+///////////////////////////////////////
+
+void Star::SetMass (double m) {
+	mass = m;
+}
+
+void Star::SetAge (double a) {
+	age = a;
+}
+
+void Star::SetMetallicity (double met) {
+	metallicity = met;
+}
+
+void Star::SetTemperature (double t) {
+	temperature = t;
+}
+
+void Star::SetLuminosity (double l) {
+	luminosity = l;
+}
+
+void Star::SetRadius (double r) {
+	radius = r;
+}
+
+///////////////////////////////////////
+// OPERATORS
+///////////////////////////////////////
+
+Star & Star::operator=(const Star & rhs) {
+	if (this == &rhs) return *this; // handle self assignment
+	//assignment operator
+	return *this;
+}
+
+bool operator==(Star a, Star b) {
+	return (a.mass == b.mass) && (a.age == b.age) && (a.metallicity == b.metallicity) && (a.temperature == b.temperature) && (a.luminosity == b.luminosity) && (a.radius == b.radius);
+}
+
+/*
+ostream& operator<<(ostream & outs, const Star & star)
+{
+    //string L =
+    outs << star.GetSpectralClass();
+    outs << star.GetSubClass();
+    outs << star.GetLuminosityClass();
+}*/
---
+++ Star.h
@@ -0,0 +1,42 @@
+#ifndef STAR_H
+#define STAR_H
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Star
+{
+	public:
+		// Constructors
+		Star();
+		Star(double m);
+		Star(const StarData & other);
+		// Accessors
+		double GetMass ();
+		double GetAge ();
+		double GetMetallicity ();
+		double GetTemperature ();
+		double GetLuminosity ();
+		double GetRadius ();
+		// Mutators
+		void SetMass (double m);
+		void SetAge (double a);
+		void SetMetallicity (double met);
+		void SetTemperature (double t);
+		void SetLuminosity (double l);
+		void SetRadius (double r);
+		// Operators
+		Star & operator=(const Star & rhs);
+		bool operator==(Star a, Star b)
+	protected:
+	private:
+		double mass;
+		double age;
+		double metallicity;
+		double temperature;
+		double luminosity;
+		double radius;		
+};
+
+#endif // STAR_H
