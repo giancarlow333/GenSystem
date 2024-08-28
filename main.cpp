f835d27a6166e7402b2fffa842821ed1781ee0c3
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2024-08-27 20:51:26 -0700

    INCREMENT VERSION_NUMBER (0.10)

--- main.cpp
+++ main.cpp
@@ -34,7 +34,7 @@ vector<Planet> formPlanets (Star & s, default_random_engine & e, double forbidde
 void printPlanetaryClass (PlanetClass pc, string & className, string & imgFileName);
 
 // constants
-const string VERSION_NUMBER = "0.8";
+const string VERSION_NUMBER = "0.10";
 
 // struct for overall separation
 struct OverallSeparation {
